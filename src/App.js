658964e787d3a8033e96fc03a88a20968678162a
a-sha0234 <asharma_31@hotmail.com>
2021-12-17 01:25:38 +0000

    finish basic functionality of the general componen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import General from "./components/General";
 import "./style/style.scss";
-
 function App() {
   return (
     <div className="App">
--- src/components/General.js
+++ src/components/General.js
@@ -28,55 +28,76 @@ export default function General() {
     });
   }
 
+  const style = {
+    display: toggle === false ? "block" : "none",
+  };
+
+  const styletogg = {
+    display: toggle === true ? "block" : "none",
+  };
+
   return (
-    <form>
-      <TextField
-        color="primary"
-        focused
-        label="firstname"
-        type="text"
-        name="firstName"
-        value={formData.firstName}
-        placeholder="enter first name"
-        onChange={handleChange}
-      />
+    <div>
+      <form>
+        <div className="wrapper" style={style}>
+          <TextField
+            color="primary"
+            focused
+            label="firstname"
+            type="text"
+            name="firstName"
+            value={formData.firstName}
+            placeholder="enter first name"
+            onChange={handleChange}
+          />
 
-      <TextField
-        color="primary"
-        focused
-        label="lastname"
-        type="text"
-        name="secondName"
-        value={formData.secondName}
-        placeholder="enter last name"
-        onChange={handleChange}
-      />
+          <TextField
+            color="primary"
+            focused
+            label="lastname"
+            type="text"
+            name="secondName"
+            value={formData.secondName}
+            placeholder="enter last name"
+            onChange={handleChange}
+          />
 
-      <TextField
-        color="primary"
-        focused
-        label="number"
-        type="number"
-        name="PhoneNum"
-        value={formData.PhoneNum}
-        placeholder="enter number"
-        onChange={handleChange}
-        className="textfields"
-      />
-      <TextField
-        type="text"
-        label="email"
-        color="primary"
-        focused
-        placeholder="enter email"
-        name="emailAddress"
-        value={formData.emailAddress}
-        onChange={handleChange}
-        className="textfields"
-      />
-      <Button variant="outlined" size="large" onClick={btnToggle}>
-        Save
-      </Button>
-    </form>
+          <TextField
+            color="primary"
+            focused
+            label="number"
+            type="number"
+            name="PhoneNum"
+            value={formData.PhoneNum}
+            placeholder="enter number"
+            onChange={handleChange}
+          />
+          <TextField
+            type="text"
+            label="email"
+            color="primary"
+            focused
+            placeholder="enter email"
+            name="emailAddress"
+            value={formData.emailAddress}
+            onChange={handleChange}
+          />
+          <Button variant="outlined" size="large" onClick={btnToggle}>
+            Save
+          </Button>
+        </div>
+      </form>
+
+      <div className="formInfo" style={styletogg}>
+        <h1>General Information </h1>
+        <p>First Name: {formData.firstName}</p>
+        <p>Last Name: {formData.secondName}</p>
+        <p>Phone number: {formData.PhoneNum}</p>
+        <p> Email address: {formData.emailAddress}</p>
+        <Button variant="outlined" size="large" onClick={btnToggle}>
+          Edit
+        </Button>
+      </div>
+    </div>
   );
 }
